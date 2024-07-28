85a983afa54cfb9dc9b8a4a5775c1791d088844f
uday <121241786+uday1508@users.noreply.github.com>
2024-07-28 15:51:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const todoRoutes = require('./routes/todoRoutes'); // import todo routes
 const userRoutes = require('./routes/userRoutes'); // import user routes
 
 const app = express(); // create express app
-const port = 8080; // port to listen on
+const PORT = "0.0.0.0"; // port to listen on
 
 app.use(cors()); // use cors
 app.use(express.json()); // use express.json to parse json bodies
@@ -32,6 +32,6 @@ app.use('/api/todos', todoRoutes);
 app.use('/api/users', userRoutes);
 
 // Start server
-// app.listen(port, () => {
-//   console.log(`Server listening at http://localhost:${port}`);
-// });
+app.listen( PORT, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
